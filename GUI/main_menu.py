ca15ce6188a34ccc8e7e71d39241fe949acae5da
siewhean <tohsh17@gmail.com>
2022-11-20 23:50:56 +0800

    Updated Main Menu

--- GUI/main_menu.py
+++ GUI/main_menu.py
@@ -4,7 +4,6 @@ FR_PRIVATE = 0x10
 FR_NOT_ENUM = 0x20
 import tkinter as tk
 
-
 ##################################################### Import Font #####################################################
 def loadfont(fontpath, private=True, enumerable=True):
     if isinstance(fontpath, bytes):
@@ -21,7 +20,6 @@ def loadfont(fontpath, private=True, enumerable=True):
     flags = (FR_PRIVATE if private else 0) | (FR_NOT_ENUM if not enumerable else 0)
     numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
     return bool(numFontsAdded)
-
 
 font = loadfont("LoRes.ttf")
 root = tk.Tk()
@@ -47,25 +45,26 @@ font_size_offset = 5
 back_button_height = 50
 back_button_y_offset = 10
 
-############################################# Insert Target Heading Value #############################################
+##################################################### Insert Title #####################################################
 Title = tk.Label(root, text='YOUR DAYS ARE\nNUMBERED', font=("LoRes 9 Plus OT Wide", 60), fg="white", bg="black",
-                                padx=-10)
+                 padx=-10)
 Title.pack(pady=(y_padding_from_top_window, 0))
 Title.pack()
 
 ################################################## Insert Play Button ##################################################
-play_button_frame = tk.Frame(root)
-play_button_frame.configure(bg="black")
-play_button_frame.pack(pady=100)
+button_frame = tk.Frame(root)
+button_frame.configure(bg="black")
+button_frame.pack(pady=100)
 
-Play_Button = tk.Button(play_button_frame, text="PLAY", font=("LoRes 9 Plus OT Wide", 24), fg="white", bg="black", borderwidth=10, highlightbackground="white",
-                      command=lambda: next())
+Play_Button = tk.Button(button_frame, text="PLAY", font=("LoRes 9 Plus OT Wide", 24), fg="white", bg="black",
+                        borderwidth=10, highlightbackground="white",
+                        command=lambda: next())
 Play_Button.grid(row=0, column=0, padx=50)
 
 ################################################## Insert Shop Button ##################################################
-Shop_Button = tk.Button(play_button_frame, text="SHOP", font=("LoRes 9 Plus OT Wide", 24), fg="white", bg="black", borderwidth=10, highlightbackground="white",
-                      command=lambda: shop())
+Shop_Button = tk.Button(button_frame, text="SHOP", font=("LoRes 9 Plus OT Wide", 24), fg="white", bg="black",
+                        borderwidth=10, highlightbackground="white",
+                        command=lambda: shop())
 Shop_Button.grid(row=0, column=1, padx=50)
-
 
 root.mainloop()
